6f97401f1b0d652b908483845732ec6611b89111
Haeparic <v567v202@gmail.com>
2023-02-22 10:02:37 +0900

    프로젝트 기본 작업 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,45 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () => {
+const Header = ({ setPage, open, setOpen }) => {
   return (
     <header className="header">
       <div className="inner">
         <nav className="nav">
-          <span className="header-logo">Haeparic Profile</span>
+          <span className="header-logo" onClick={() => setPage(0)}>
+            Haeparic Profile
+          </span>
           <ul className="gnb">
-            <li>
+            <li onClick={() => setPage(0)}>
               <span>Home</span>
             </li>
-            <li>
+            <li onClick={() => setPage(1)}>
               <span>Profile</span>
             </li>
-            <li>
+            <li onClick={() => setPage(2)}>
               <span>Skill</span>
             </li>
-            <li>
+            <li onClick={() => setPage(3)}>
               <span>Portfolio</span>
             </li>
-            <li>
+            <li onClick={() => setPage(4)}>
               <span>Life</span>
             </li>
-            <li>
+            <li onClick={() => setPage(5)}>
               <span>Vision</span>
             </li>
           </ul>
-          <span className="mobile-bt">
-            <FontAwesomeIcon icon={faBars} />
+          <span
+            className="mobile-bt"
+            onClick={() => {
+              open ? setOpen(false) : setOpen(true);
+            }}
+          >
+            {open ? (
+              <FontAwesomeIcon icon={faXmark} />
+            ) : (
+              <FontAwesomeIcon icon={faBars} />
+            )}
           </span>
         </nav>
       </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,18 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import required modules
+import { Pagination, Navigation } from "swiper";
 
 const Portfolio = () => {
   return (
-    <section className="portfolio">
+    <section className="portfolio scroll">
       <div className="inner">
-        <h2>Portfolio</h2>
+        {/* 개발담당자 : 팀프로젝트, 개인 작업(5개 목표) */}
+        <h2 data-aos="fade-up">Portfolio</h2>
         <div className="contents">
           {/* 팀프로젝트 */}
           <div className="portfolio-box">
@@ -12,7 +20,7 @@ const Portfolio = () => {
             <div className="portfolio-team">
               <div className="portfolio-left">
                 <a
-                  href="#"
+                  href="https://haeparic.github.io/Hansalim/"
                   target="_blank"
                   alt="portfolio"
                   rel="noopener noreferrer"
@@ -30,7 +38,17 @@ const Portfolio = () => {
                   <li>프로젝트 도구 : </li>
                   <li>프로젝트 사용언어 : </li>
                   <li>프로젝트 피그마 : </li>
-                  <li>프로젝트 Git : </li>
+                  <li>
+                    프로젝트 Git :{" "}
+                    <a
+                      href="https://github.com/Haeparic/Hansalim/"
+                      target="_blank"
+                      alt="portfolio"
+                      rel="noopener noreferrer"
+                    >
+                      작업물보기
+                    </a>{" "}
+                  </li>
                   <li>프로젝트 Notion : </li>
                 </ul>
               </div>
@@ -117,48 +135,101 @@ const Portfolio = () => {
           {/* 개인 스터디 프로젝트 */}
           <div className="portfolio-box">
             <h3>Study Project</h3>
-            <ul className="portfolio-list">
-              <li>
+            {/* Swiper 배치 */}
+            <Swiper
+              loop={true}
+              slidesPerView={1}
+              spaceBetween={0}
+              pagination={true}
+              navigation={true}
+              breakpoints={{
+                860: {
+                  slidesPerView: 1,
+                  spaceBetween: 0,
+                },
+                960: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                },
+                1180: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+              }}
+              modules={[Pagination, Navigation]}
+              className="portfolio-slide"
+            >
+              <SwiperSlide>
                 <a
                   href="#"
                   target="_blank"
-                  alt="portfolio"
                   rel="noopener noreferrer"
+                  alt="portfolio"
                 >
                   <img src="/images/sample.png" alt="portfolio" />
                 </a>
-              </li>
-              <li>
+              </SwiperSlide>
+              <SwiperSlide>
                 <a
                   href="#"
                   target="_blank"
-                  alt="portfolio"
                   rel="noopener noreferrer"
+                  alt="portfolio"
                 >
                   <img src="/images/sample.png" alt="portfolio" />
                 </a>
-              </li>
-              <li>
+              </SwiperSlide>
+              <SwiperSlide>
                 <a
                   href="#"
                   target="_blank"
-                  alt="portfolio"
                   rel="noopener noreferrer"
+                  alt="portfolio"
                 >
                   <img src="/images/sample.png" alt="portfolio" />
                 </a>
-              </li>
-              <li>
+              </SwiperSlide>
+              <SwiperSlide>
                 <a
                   href="#"
                   target="_blank"
-                  alt="portfolio"
                   rel="noopener noreferrer"
+                  alt="portfolio"
                 >
                   <img src="/images/sample.png" alt="portfolio" />
                 </a>
-              </li>
-            </ul>
+              </SwiperSlide>
+              <SwiperSlide>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="portfolio"
+                >
+                  <img src="/images/sample.png" alt="portfolio" />
+                </a>
+              </SwiperSlide>
+              <SwiperSlide>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="portfolio"
+                >
+                  <img src="/images/sample.png" alt="portfolio" />
+                </a>
+              </SwiperSlide>
+              <SwiperSlide>
+                <a
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  alt="portfolio"
+                >
+                  <img src="/images/sample.png" alt="portfolio" />
+                </a>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </div>
